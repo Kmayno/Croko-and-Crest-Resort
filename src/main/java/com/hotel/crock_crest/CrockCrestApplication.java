ad5559b1b7afe603fe2a4b110aaa76e94e4a876f
Kmayno <carminenigro4@gmail.com>
2025-09-03 17:31:26 +0200

    Initial commit

---
+++ src/main/java/com/hotel/crock_crest/CrockCrestApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.crock_crest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrockCrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrockCrestApplication.class, args);
+    }
+
+}
