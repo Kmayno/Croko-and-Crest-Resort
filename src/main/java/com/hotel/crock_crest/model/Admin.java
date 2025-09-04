623dc2543ea18ff2b0989f6d4765c993306877e2
Kmayno <carminenigro4@gmail.com>
2025-09-04 17:27:45 +0200

    push ripristino admin

--- src/main/java/com/hotel/crock_crest/model/Admin.java
+++ src/main/java/com/hotel/crock_crest/model/Admin.java
@@ -1,25 +1,54 @@
 package com.hotel.crock_crest.model;
 
 import jakarta.persistence.*;
+
 @Entity
-@Table(name = "camere")
-public class Camera {
+@Table(name = "admin")
+public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idCamera;
+    @Column(name ="id_admin")
+    private int idAdmin;
 
-    private String numeroStanza;
-    private String tipoCamera;
-    private String descrizione;
+    @Column(name = "nome")
+    private String nome;
 
-    private Double prezzoBaseNotte;
-    private Integer capienzaMassima;
+    @Column(name = "password_admin")
+    private String passwordAdmin;
+    @Column(name = "email")
+    private String email;
 
-    private Boolean disponibile;
+    public Admin(){}
 
-    @OneToMany(mappedBy = "camera", cascade = CascadeType.ALL)
-    private List<InventarioCamera> inventario;
+    public int getIdAdmin() {
+        return idAdmin;
+    }
 
-    @OneToMany(mappedBy = "camera", cascade = CascadeType.ALL)
-    private List<Prenotazione> prenotazioni;
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPasswordAdmin() {
+        return passwordAdmin;
+    }
+
+    public void setPasswordAdmin(String passwordAdmin) {
+        this.passwordAdmin = passwordAdmin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
