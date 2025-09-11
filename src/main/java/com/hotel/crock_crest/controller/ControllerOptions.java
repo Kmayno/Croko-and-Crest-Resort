ddd0cffd4d67027a17612498668e5370e15b7cb9
Kmayno <carminenigro4@gmail.com>
2025-09-11 15:48:46 +0200

    fatto robe, tante

--- src/main/java/com/hotel/crock_crest/controller/ControllerOptions.java
+++ src/main/java/com/hotel/crock_crest/controller/ControllerOptions.java
@@ -3,11 +3,7 @@ package com.hotel.crock_crest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hotel.crock_crest.model.OpzionePersonalizzazione;
 import com.hotel.crock_crest.service.OptionService;
@@ -15,6 +11,7 @@ import com.hotel.crock_crest.service.OptionService;
 
 @RestController
 @RequestMapping("/api/options")
+@CrossOrigin(origins = "*")
 public class ControllerOptions {
 
     @Autowired
